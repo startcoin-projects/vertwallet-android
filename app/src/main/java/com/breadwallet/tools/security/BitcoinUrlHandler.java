7be0972aa6bd9295b25021a70bd06594df3bceae
Philippe Michaud-Boudreault <pitwuu@gmail.com>
2018-01-04 22:09:23 -0500

    Fix the URL handling for scanning, QR.
    * bitcoin: -> vertcoin:

--- app/src/main/java/com/breadwallet/tools/security/BitcoinUrlHandler.java
+++ app/src/main/java/com/breadwallet/tools/security/BitcoinUrlHandler.java
@@ -120,11 +120,11 @@ public class BitcoinUrlHandler {
 
         String tmp = str.trim().replaceAll("\n", "").replaceAll(" ", "%20");
 
-        if (!tmp.startsWith("bitcoin://")) {
-            if (!tmp.startsWith("bitcoin:"))
-                tmp = "bitcoin://".concat(tmp);
+        if (!tmp.startsWith("vertcoin://")) {
+            if (!tmp.startsWith("vertcoin:"))
+                tmp = "vertcoin://".concat(tmp);
             else
-                tmp = tmp.replace("bitcoin:", "bitcoin://");
+                tmp = tmp.replace("vertcoin:", "vertcoin://");
         }
         URI uri = URI.create(tmp);
 
